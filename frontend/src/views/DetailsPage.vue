0907d577b32e996311d9d133d364adcf302fab04
nirtal <nirt04@gmail.com>
2019-03-19 23:43:14 +0200

    movie container

--- frontend/src/views/DetailsPage.vue
+++ frontend/src/views/DetailsPage.vue
@@ -1,18 +1,9 @@
-
 <template>
-  <div class="details">
-    <h1>Nachos details</h1>
-    <div class="movie-container">
-      <img :src="movie.img.poster">
-      <div class="movie-details">
-        <h1>{{movie.details.title}}</h1>
-        <p>{{movie.details.overview}}</p>
-      </div>
-    </div>
-  </div>
+<movie-container :movie="movie"></movie-container>
 </template>
 
 <script>
+import movieContainer from './components/details-cmps/movie-container.vue'
 const utility = require("../services/UtilityService.js");
 export default {
   data() {
@@ -26,41 +17,26 @@ export default {
     };
   },
   async created() {
+    this.testInit();
     //  const movieId = this.$route.params.movieId
-    const movieId = 299537;
-    const details = await this.$store.dispatch("getMovieDetails", movieId);
-    const imgs = await this.$store.dispatch("getMovieImages", movieId);
-    this.movie.img.poster = utility.imgURL(imgs.posters[0].file_path)
-    this.movie.details = details;
-    // this.movie.img.poster = utility.imgURL(
-    //   details.belongs_to_collection.poster_path
-    // );
-  }
+  },
+  methods: {
+    async testInit() {
+      const movieId = 299537;
+      const details = await this.$store.dispatch("getMovieDetails", movieId);
+      const imgs = await this.$store.dispatch("getMovieImages", movieId);
+      this.movie.img.poster = utility.imgURL(imgs.posters[0].file_path);
+      this.movie.details = details;
+      console.log(details);
+    }
+  },
+  components: {
+  movieContainer
+}
 };
+
 </script>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
-.movie-container {
-    margin: 10% auto;
-    border-radius: 5px;
-    padding: 20px;
-    max-width: 60%;
-    background-color: lightslategray;
-    display: flex;
-}
-.movie-container > img {
-  margin-right: 20px;
-}
-.movie-details {
-  display: flex;
-  flex-direction: column;
-}
-.movie-details > p {
-  margin-top: 20px;
-}
-</style>
 
+</style>
---
+++ frontend/src/views/components/details-cmps/movie-container.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="details">
+    <h1>Nachos details</h1>
+    <div class="movie-container">
+      <img :src="movie.img.poster">
+      <div class="movie-details">
+        <h1>{{movie.details.title}}</h1>
+        <h4>Release date: {{movie.details.release_date}}</h4>
+        <p>{{movie.details.overview}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['movie'],
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.movie-container {
+  margin: 10% auto;
+  border-radius: 5px;
+  padding: 20px;
+  max-width: 60%;
+  background-color: lightslategray;
+  display: flex;
+}
+.movie-container > img {
+  margin-right: 20px;
+}
+.movie-details {
+  display: flex;
+  flex-direction: column;
+}
+.movie-details > p {
+  margin-top: 20px;
+}
+</style>
