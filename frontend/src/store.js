386652c73fb9a29b4dc5958560ecb621896a8f6f
Guy <nevandnoiz@gmail.com>
2019-03-19 17:29:16 +0200

    create front

---
+++ frontend/src/store.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+// Import modules here. ex:
+// import UserModule from './modules/user.js'
+
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+  // Write your modules here
+  },
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+  <h1>Nachos home</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {
+    
+  }
+}
+</script>
