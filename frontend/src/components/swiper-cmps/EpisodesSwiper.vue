7f7c09ea9d90c096ab3fd7bde207784e616ad82a
nirtal <nirt04@gmail.com>
2019-03-27 10:30:58 +0200

    css episodes check buttons

--- frontend/src/components/swiper-cmps/EpisodesSwiper.vue
+++ frontend/src/components/swiper-cmps/EpisodesSwiper.vue
@@ -1,13 +1,17 @@
 <template>
   <section v-if="this.season.episodes" class="slider-main-container">
-    <h1></h1>
     <carousel :paginationEnabled="false" :navigationEnabled="true" :per-page="5">
       <slide
         class="slider-card"
         :style="{'background-image':'url(\''+imgURL(eposide.still_path)+'\')'}"
         v-for="(eposide, index) in season.episodes"
         :key="index"
-      ></slide>
+      >
+        
+<div class="check-container"><i class="fas fa-check"></i></div>
+        <h1>{{eposide.episode_number}}</h1>
+        <p>{{eposide.name}}</p>
+      </slide>
     </carousel>
   </section>
 </template>
@@ -24,7 +28,8 @@ export default {
       id: this.tvShowId,
       seasons: this.seasons
     });
-    this.onEmit(0)
+    this.onEmit(0);
+    console.log(this.season.episodes);
   },
   components: {},
   data() {
@@ -46,6 +51,54 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  color: white;
+  margin: 0.1rem 0.5rem;
+  align-self: flex-start;
+  order: -1;
+  font-size: 1.8rem;
+}
+
+p {
+  color: white;
+  align-self: end;
+  margin: 0.2rem;
+}
+i {
+ font-size: 42px;
+    margin: 9px 9px 9px 0;
+    background-color: white;
+    opacity: 0.5;
+    color: darkslategray;
+    padding: 8px;
+    /* height: 57px; */
+    border-radius: 50%;
+    flex-grow: 0;
+    -webkit-transition: 0.2s;
+    transition: 0.2s;
+}
+i:hover {
+  cursor: pointer;
+  opacity: 0.75;
+}
+.check-container {
+      position: absolute;
+    /* margin: 50%; */
+    top: 0;
+    /* left: 0; */
+    /* right: 0; */
+    margin: auto 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    /* flex-grow: unset; */
+    bottom: 0;
+}
+.checked {
+  background-color: rgb(1, 221, 1);
+}
+
 .slider-main-container {
   z-index: 3423423423;
 }
@@ -54,11 +107,18 @@ export default {
   height: 300px;
 }
 .slider-card {
-  background-color: black;
-  background-size: 287px;
-  height: 161px;
-  border: 1px solid;
-  background-repeat: no-repeat;
+    background-color: black;
+    background-size: 287px;
+    height: 161px;
+    position: relative;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    border: 1px solid;
+    background-repeat: no-repeat;
 }
 
 .cards-container-of-season-netflix {
