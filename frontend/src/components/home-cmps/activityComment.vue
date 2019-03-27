e92a61e5ee626f435c03c93d6ae61168d6fd59ff
Oded <oded1188@me.com>
2019-03-27 19:05:33 +0200

    activity comment design

--- frontend/src/components/home-cmps/activityComment.vue
+++ frontend/src/components/home-cmps/activityComment.vue
@@ -1,14 +1,54 @@
 <template>
-    <section>
-        <div>
-            <p>{{comment.comment}}</p>
-        </div>
-    </section>
+  <section>
+    <div class="container">
+      <div class="user">
+        <img src alt>
+        <p class="user-name">{{userName}}</p>
+      </div>
+
+      <div>
+        <p class="comment">{{comment.comment}}</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 
 <script>
 export default {
-    props: ['comment']
-}
+  props: ["comment"],
+  computed: {
+    userName: function() {
+      return (
+        this.comment.user.name.firstName + " " + this.comment.user.name.lastName
+      );
+    }
+  }
+};
 </script>
+
+
+
+<style lang="scss" scoped>
+.container {
+    margin-top: 15px;
+  .user {
+    display: flex;
+    align-items: center;
+    img {
+      min-width: 25px;
+      min-height: 25px;
+      border: 1px solid gray;
+      border-radius: 50%;
+    }
+    .user-name {
+      text-transform: capitalize;
+      margin-left: 5px;
+    }
+  }
+
+  .comment {
+    margin-left: 45px;
+  }
+}
+</style>
