7c6aab58efe599d5c6065753b82173778c20a630
Guy <nevandnoiz@gmail.com>
2019-04-01 18:33:33 +0300

    sign out

--- frontend/src/components/header-cmps/AppHeader.vue
+++ frontend/src/components/header-cmps/AppHeader.vue
@@ -63,7 +63,6 @@ export default {
     },
     async signOut() {
       let signOut = await this.$store.dispatch("signOut");
-      console.log("comp");
       this.$router.push("/");
     },
     toggleModal() {
