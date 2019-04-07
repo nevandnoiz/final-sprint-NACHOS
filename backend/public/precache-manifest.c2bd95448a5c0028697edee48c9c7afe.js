551850180880054c8833d1f2302d7f2fe8a836ce
Guy <nevandnoiz@gmail.com>
2019-04-07 15:46:47 +0300

    will you work?

---
+++ backend/public/precache-manifest.c2bd95448a5c0028697edee48c9c7afe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "98edf43fdc849716c43ce794a23b50e9",
+    "url": "/img/Nachos-icon.98edf43f.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a497ce3c62afda6aded2",
+    "url": "/js/chunk-vendors.0394069a.js"
+  },
+  {
+    "revision": "6378d04d6266f72b909c",
+    "url": "/js/app.35563e74.js"
+  },
+  {
+    "revision": "e0d47b4998e218ca0a1e742a94f45b10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bbc733d331decd7619c31fc5c6d9f8b",
+    "url": "/img/netflix.7bbc733d.svg"
+  },
+  {
+    "revision": "2689dc621751be7e5f006290b22982ca",
+    "url": "/img/itunes.2689dc62.svg"
+  },
+  {
+    "revision": "8a55203e795561ed4d88ad6c49cc13fc",
+    "url": "/img/blinkbox.8a55203e.svg"
+  },
+  {
+    "revision": "7c17b7ac416894c11ac36f9ee38603b5",
+    "url": "/img/amazon.7c17b7ac.svg"
+  },
+  {
+    "revision": "81a66bb4910734f0ee2787bd5302c48b",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "6378d04d6266f72b909c",
+    "url": "/css/app.538ae368.css"
+  }
+];
